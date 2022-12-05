4d5a49e8a0fb7617d86c38716115eba16e7f6356
Maxime Vincent <maxime.vincent1@universite-paris-saclay.fr>
2022-12-05 16:49:05 +0100

    Permettre la connexion simultanée de plusieurs clients au serveur

--- irc.py
+++ irc.py
@@ -5,12 +5,14 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.connect(("localhost", 9999))
 
-def run():
+# On attend que le serveur nous envoie des messages
+def recv_msg():
     while True:
         msg = s.recv(1024)
         print('\r'+msg.decode('utf-8'), end='\n> ')
 
-threading.Thread(target=run, daemon=True).start()
+threading.Thread(target=recv_msg, daemon=True).start()
 
+# On envoie des commandes au serveur
 while True:
-    s.send(input("> ").encode("utf-8"))
+    s.send(input("> ").lower().strip().encode("utf-8"))
--- server.py
+++ server.py
@@ -1,19 +1,52 @@
 import socket
 import threading
 import sys
+import time
+
+help = \
+"""
+/away [message]  Signale son absence quand on nous envoie un message en privé
+                 (en réponse un message peut être envoyé).
+                 Une nouvelle commande /away réactive l’utilisateur.
+
+/help  Affiche la liste des commandes disponibles
+
+/invite <nick>  Invite un utilisateur sur le canal où on se trouve
+
+/join <canal> [clé]  Permet de rejoindre un canal (protégé éventuellement par une clé).
+                     Le canal est créé s’il n’existe pas.
+
+/list  Affiche la liste des canaux sur IRC
+
+/msg [canal|nick] message  Pour envoyer un message à un utilisateur ou sur un canal (où on est
+                           présent ou pas). Les arguments canal ou nick sont optionnels.
+
+/names [channel]  Affiche les utilisateurs connectés à un canal. Si le canal n’est pas spécifié,
+                  affiche tous les utilisateurs de tous les canaux.
+"""
+
+default_error = "DEFAULT_ERROR"
+
+def exec_cmd(sc):
+    while True:
+        # Comment déterminer la taille adéquate ?
+        # Comment savoir si le message dépasse la taille maximale ?
+        # sys.getsizeof(...)
+        cmd = sc.recv(1024).decode('utf-8')
+        # Exécution de la commande
+        if cmd.startswith("/help"):
+            sc.send(help.encode('utf-8'))
+        else:
+            sc.send(default_error.encode('utf-8'))
+        # On fermera la connexion avec le client dans l'architecture pair à pair
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(("localhost", 9999))
-s.listen(1)
-sc, ip_client = s.accept()
-
-def run():
-    while True:
-        msg = sc.recv(1024)
-        print('\r'+msg.decode('utf-8'), end='\n> ')
-
-threading.Thread(target=run, daemon=True).start()
-
+# Le serveur peut traiter jusqu'à 100 connexions
+s.listen(100)
+# Attente de clients
 while True:
-    sc.send(input("> ").encode("utf-8"))
+    sc, ip_client = s.accept()
+    # Lorsqu'on accepte un client on traite sa requête dans un thread séparé
+    threading.Thread(target=exec_cmd, args=(sc,)).start()
